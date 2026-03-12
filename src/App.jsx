46748804ead73b3eaaf60aab03446015877d6e87
angeloren92 <angeloren92@gmail.com>
2026-03-12 15:36:10 +0100

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import AppFooter from './components/AppFooter'
 function App() {
 
   const recipes = [
-  { id: 1, title: "Spaghetti alla Carbonara: la ricetta originale" },
-  { id: 2, title: "Tiramisù fatto in casa in 5 semplici passi" },
-  { id: 3, title: "Risotto ai funghi porcini e parmigiano" },
-  { id: 4, title: "Pizza Margherita: i segreti per un impasto perfetto" },
-  { id: 5, title: "Pollo al forno con patate croccanti" }
-];
+    "Spaghetti alla Carbonara: la ricetta originale",
+    "Tiramisù fatto in casa in 5 semplici passi",
+    "Risotto ai funghi porcini e parmigiano",
+    "Pizza Margherita: i segreti per un impasto perfetto",
+    "Pollo al forno con patate croccanti" 
+  ];
 
   return (
     <>
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react"
+
 function AppMain({ recipes }) {
+
+  const [newItem, setItems] = useState('')
+  const [newList, setNewList] = useState(recipes)
+  const [error, setError] = useState(false)
+
+  function addNewItem(e) {
+    e.preventDefault()
+    if (newItem.length >= 10) {
+      setNewList([newItem, ...newList])
+      setError(false)
+    } else {
+      setError(true)
+    }
+  }
 
   return (
     <main className="container">
       <div className="row my-3">
         <div className="col">
-          <ul class="list-group">
-            {recipes.map(({title, id}) => (
-              <li key={id} class="list-group-item">{title}</li>
+          <form className="input-group mt-3" onSubmit={addNewItem}>
+            <input type="text" className="form-control" placeholder="Aggiungi Nuova Sezione" value={newItem} onChange={(e) => setItems(e.target.value)}/>
+            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Aggiungi</button>
+          </form>
+          {error && <small className="text-danger m-1">INSERISCI ALMENO 10 CARATTERI!</small>}
+          <ul className="list-group my-3">
+            {newList.map( (element, index) => (
+              <li key={index} className="list-group-item">{element}</li>
             ))
             }
           </ul>
