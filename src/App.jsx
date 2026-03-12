7c25d067cd80ae0a3dce60f106ed484742ca9b02
angeloren92 <angeloren92@gmail.com>
2026-03-12 14:42:03 +0100

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import AppFooter from './components/AppFooter'
 
 function App() {
 
+  const recipes = [
+  { id: 1, title: "Spaghetti alla Carbonara: la ricetta originale" },
+  { id: 2, title: "Tiramisù fatto in casa in 5 semplici passi" },
+  { id: 3, title: "Risotto ai funghi porcini e parmigiano" },
+  { id: 4, title: "Pizza Margherita: i segreti per un impasto perfetto" },
+  { id: 5, title: "Pollo al forno con patate croccanti" }
+];
+
   return (
     <>
       <AppHeader />
-      <AppMain />
+      <AppMain recipes={recipes} />
       <AppFooter />
     </>
   )
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,9 +1,18 @@
-function AppMain() {
+function AppMain({ recipes }) {
 
   return (
-    <>
-
-    </>
+    <main className="container">
+      <div className="row my-3">
+        <div className="col">
+          <ul class="list-group">
+            {recipes.map(({title, id}) => (
+              <li key={id} class="list-group-item">{title}</li>
+            ))
+            }
+          </ul>
+        </div>
+      </div>
+    </main>
   )
 }
 
